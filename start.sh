a8b5b24aae253e5f93a43da237e96d3e84c0576e
eldritchjs <jschlessman@gmail.com>
2018-03-26 15:52:43 -0400

    Removed infinispan.json

--- start.sh
+++ start.sh
@@ -26,7 +26,16 @@ oc new-app --template=amq63-basic \
 
 oc create -f https://radanalytics.io/resources.yaml
 
-oc create -f infinispan.json
+oc create -f https://raw.githubusercontent.com/infinispan/infinispan-openshift-templates/master/templates/infinispan-ephemeral.json
+
+oc new-app --template=infinispan-ephemeral \
+	-l app=datagrid \
+	-p APPLICATION_NAME=datagrid \
+	-p NAMESPACE=$PROJECT_NAME \
+	-p APPLICATION_USER=daikon \
+	-p APPLICATION_PASSWORD=daikon \
+	-p MANAGEMENT_USER=daikon \
+	-p MANAGEMENT_PASSWORD=daikon 
 
 oc new-app --template=oshinko-scala-spark-build-dc \
 	-l app=handler \
@@ -37,8 +46,11 @@ oc new-app --template=oshinko-scala-spark-build-dc \
 	-p GIT_REF=ivupdate \
 	-p APP_MAIN_CLASS=io.radanalytics.equoid.DataHandler \
 	-e jdgHost=datagrid-hotrod \
-	-e jdgPort=11333 \
+	-e jdgPort=11222 \
 	-p SPARK_OPTIONS='--driver-java-options=-Dvertx.cacheDirBase=/tmp'
 
-oc new-app --allow-missing-imagestream-tags -l app=publisher redhat-openjdk18-openshift:1.2~https://github.com/eldritchjs/equoid-data-publisher 
+oc new-app --allow-missing-imagestream-tags \
+	-l app=publisher \
+	--image-stream=$PROJECT_NAME/redhat-openjdk18-openshift:1.2 \
+	https://github.com/eldritchjs/equoid-data-publisher 
 
