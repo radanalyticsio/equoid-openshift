a5fbb4192a3f4a89e707d598fccab8ae393aa033
eldritchjs <jschlessman@gmail.com>
2018-03-26 14:31:30 -0400

    Updated startup script for AIO cluster deployment

--- start.sh
+++ start.sh
@@ -1,48 +1,44 @@
 #!/bin/bash
-#docker kill `docker ps -q` || true
-#oc cluster up
-#oc login -u system:admin
-#oc create -n openshift -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json
-#oc create -n openshift -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/amq/amq63-basic.json
-#oc login -u developer
-#oc create -f jboss-image-streams.json
-#oc create -f amq63-basic.json
-oc new-project equoid-auto
-oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json
+
+PROJECT_NAME=equoid
+
+if [ $# -gt 1 ];
+then
+	echo "Usage: $0 [project name]"
+	exit
+fi
+
+if [ $# -gt 0 ];
+then
+	PROJECT_NAME=$1
+fi
+
+oc new-project $PROJECT_NAME
+oc create -f  https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json
 oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/amq/amq63-basic.json
 
 oc new-app --template=amq63-basic \
-  -l app=broker \
-  -p IMAGE_STREAM_NAMESPACE=equoid
-  --image-stream=jboss-amq-63:1.2 \
-  -p MQ_PROTOCOL=amqp \
-  -p MQ_QUEUES=salesq \
-  -p MQ_TOPICS=salest \
-  -p MQ_USERNAME=daikon \
-  -p MQ_PASSWORD=daikon \
-  -p AMQ_MESH_DISCOVERY_TYPE=kube
-  
-oc create -f infinispan.json
+	-p MQ_PROTOCOL=amqp \
+	-p MQ_QUEUES=salesq \
+        -p MQ_USERNAME=daikon \
+	-p MQ_PASSWORD=daikon \
+        -p IMAGE_STREAM_NAMESPACE=$PROJECT_NAME	
+
 oc create -f https://radanalytics.io/resources.yaml
 
-sleep 5
-AMQP_IP=`oc describe pod -l application=broker | grep IP | awk '{split($0,a,"\t*"); print a[2]}'`
-echo broker IP: $AMQP_IP
+oc create -f infinispan.json
 
-oc new-app --template=oshinko-java-spark-build-dc \
-  -l app=handler \
-  -p OSHINKO_CLUSTER_NAME=sparky \
-  -p APPLICATION_NAME=equoid-data-handler \
-  -p GIT_URI=https://github.com/eldritchjs/equoid-data-handler \
-  -p APP_MAIN_CLASS=io.radanalytics.equoid.dataHandler \
-  -p APP_ARGS='broker-amq-amqp 5672 daikon daikon salesq datagrid-hotrod 11333 10 6 0.9' \
-  -p APP_FILE=equoid-data-handler-1.0-SNAPSHOT.jar \
-  -p SPARK_OPTIONS='--driver-java-options=-Dvertx.cacheDirBase=/tmp'
+oc new-app --template=oshinko-scala-spark-build-dc \
+	-l app=handler \
+	-p SBT_ARGS=assembly \
+	-p OSHINKO_CLUSTER_NAME=sparky \
+	-p APPLICATION_NAME=equoid-data-handler \
+	-p GIT_URI=https://github.com/eldritchjs/equoid-data-handler \
+	-p GIT_REF=ivupdate \
+	-p APP_MAIN_CLASS=io.radanalytics.equoid.DataHandler \
+	-e jdgHost=datagrid-hotrod \
+	-e jdgPort=11333 \
+	-p SPARK_OPTIONS='--driver-java-options=-Dvertx.cacheDirBase=/tmp'
 
-sleep 5
-
-oc new-app \
-	-l app=publisher \
-	--image-stream=redhat-openjdk18-openshift:1.2 \
-	https://github.com/EldritchJS/equoid-data-publisher 
+oc new-app -l app=publisher redhat-openjdk18-openshift:1.2~https://github.com/eldritchjs/equoid-data-publisher 
 
