13edcff3fe04f1d5945d593bb029ea5fcd96e8c5
eldritchjs <jschlessman@gmail.com>
2018-05-03 10:04:29 -0400

    Changed data handler creation to naming convention of start-handler.sh

--- start-handler.sh
+++ start-handler.sh
@@ -9,10 +9,11 @@ oc new-app --template=oshinko-scala-spark-build-dc \
     -p OSHINKO_CLUSTER_NAME=sparky-$SECONDS \
     -p APPLICATION_NAME=equoid-data-handler-$SECONDS \
     -p GIT_URI=https://github.com/eldritchjs/equoid-data-handler \
-    -p GIT_REF=ivupdate \
+    -p GIT_REF=removesparky \
     -p APP_MAIN_CLASS=io.radanalytics.equoid.DataHandler \
     -e JDG_HOST=datagrid-hotrod \
     -e JDG_PORT=11222 \
     -e WINDOW_SECONDS=$SECONDS \
     -e SLIDE_SECONDS=$SECONDS \
+    -e BATCH_SECONDS=$SECONDS \
     -p SPARK_OPTIONS='--driver-java-options=-Dvertx.cacheDirBase=/tmp'
