12661439935fdc40fc1b2c765e9daf8eb8eb6c6e
Jason S <jschlessman@gmail.com>
2018-09-12 13:46:52 -0400

    Updated start-full script and README

--- start-full.sh
+++ start-full.sh
@@ -14,9 +14,13 @@ then
 fi
 
 oc new-project $PROJECT_NAME || oc project $PROJECT_NAME
-oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/openjdk/openjdk18-image-stream.json
+
+oc create -f https://raw.githubusercontent.com/radanalyticsio/equoid-openshift/master/openjdk18-image-stream.json
 oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/amq/amq63-image-stream.json
 oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/amq/amq63-basic.json
+oc create -f https://radanalytics.io/resources.yaml
+oc create -f https://raw.githubusercontent.com/infinispan/infinispan-openshift-templates/master/templates/infinispan-ephemeral.json
+
 
 oc new-app --template=amq63-basic \
     -l app=amqp \
@@ -25,10 +29,6 @@ oc new-app --template=amq63-basic \
     -p MQ_USERNAME=daikon \
     -p MQ_PASSWORD=daikon \
     -p IMAGE_STREAM_NAMESPACE=`oc project -q`
-
-oc create -f https://radanalytics.io/resources.yaml
-
-oc create -f https://raw.githubusercontent.com/infinispan/infinispan-openshift-templates/master/templates/infinispan-ephemeral.json
 
 oc new-app --template=infinispan-ephemeral \
     -l app=datagrid \
@@ -44,7 +44,7 @@ oc new-app --template=oshinko-scala-spark-build-dc \
     -p SBT_ARGS=assembly \
     -p APPLICATION_NAME=equoid-data-handler-20-linear \
     -p GIT_URI=https://github.com/eldritchjs/equoid-data-handler \
-    -p GIT_REF=master \
+    -p GIT_REF=DataMod \
     -p APP_MAIN_CLASS=io.radanalytics.equoid.DataHandler \
     -e JDG_HOST=datagrid-hotrod \
     -e JDG_PORT=11222 \
@@ -55,16 +55,18 @@ oc new-app --template=oshinko-scala-spark-build-dc \
     -p SPARK_OPTIONS='--driver-java-options=-Dvertx.cacheDirBase=/tmp'
 
 echo "Waiting for the imagestreamtag redhat-openjdk18-openshift:1.3"
+
+until oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/openjdk/openjdk18-image-stream.json &> /dev/null; do
 until oc get imagestreamtag/redhat-openjdk18-openshift:1.3 &> /dev/null ; do
   printf "$(tput setaf 6)▮$(tput sgr0)"
   sleep 1
-  oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/openjdk/openjdk18-image-stream.json
+  oc create -f https://raw.githubusercontent.com/radanalyticsio/equoid-openshift/master/openjdk18-image-streams.json
 done
 
 oc new-app \
     -l app=publisher \
     -e OP_MODE=linear \
-    -e DATA_URL_PRIMARY=https://raw.githubusercontent.com/EldritchJS/equoid-data-publisher/master/data/StockCodesLinear.txt \
+    -e DATA_URL_PRIMARY=https://raw.githubusercontent.com/radanalyticsio/equoid-data-publisher/master/data/StockCodesLinear.txt \
     --image-stream=`oc project -q`/redhat-openjdk18-openshift:1.3 \
     https://github.com/eldritchjs/equoid-data-publisher 
 
