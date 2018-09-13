42624c56bb937288ddee653697c2fa372281d557
eldritchjs <jschlessman@gmail.com>
2018-09-13 13:38:00 -0400

    Added Artemis config

--- start-full.sh
+++ start-full.sh
@@ -16,18 +16,10 @@ fi
 oc new-project $PROJECT_NAME || oc project $PROJECT_NAME
 
 oc create -f https://raw.githubusercontent.com/radanalyticsio/equoid-openshift/master/openjdk18-image-stream.json
-oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/amq/amq63-image-stream.json
-oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/amq/amq63-basic.json
 oc create -f https://radanalytics.io/resources.yaml
 oc create -f https://raw.githubusercontent.com/infinispan/infinispan-openshift-templates/master/templates/infinispan-ephemeral.json
 
-oc new-app --template=amq63-basic \
-    -l app=amqp \
-    -p MQ_PROTOCOL=amqp \
-    -p MQ_QUEUES=salesq \
-    -p MQ_USERNAME=daikon \
-    -p MQ_PASSWORD=daikon \
-    -p IMAGE_STREAM_NAMESPACE=`oc project -q`
+oc create -f https://raw.githubusercontent.com/radanalyticsio/equoid-openshift/master/artemis-rc.yaml
 
 oc new-app --template=infinispan-ephemeral \
     -l app=datagrid \
@@ -39,10 +31,11 @@ oc new-app --template=infinispan-ephemeral \
     -p MANAGEMENT_PASSWORD=daikon
 
 oc new-app --template=oshinko-scala-spark-build-dc \
-    -l app=handler-20-linear \
+    -l app=handler-20-artemis \
     -p SBT_ARGS=assembly \
     -p APPLICATION_NAME=equoid-data-handler-20-linear \
-    -p GIT_URI=https://github.com/radanalyticsio/equoid-data-handler \
+    -p GIT_URI=https://github.com/eldritchjs/equoid-data-handler-1 \
+    -p GIT_REF=artemis \
     -p APP_MAIN_CLASS=io.radanalytics.equoid.DataHandler \
     -e JDG_HOST=datagrid-hotrod \
     -e JDG_PORT=11222 \
@@ -65,7 +58,7 @@ oc new-app \
     -e OP_MODE=linear \
     -e DATA_URL_PRIMARY=https://raw.githubusercontent.com/radanalyticsio/equoid-data-publisher/master/data/StockCodesLinear.txt \
     --image-stream=`oc project -q`/redhat-openjdk18-openshift:1.3 \
-    https://github.com/radanalyticsio/equoid-data-publisher 
+    https://github.com/eldritchjs/equoid-data-publisher#artemis 
 
 # web-ui
 BASE_URL="https://raw.githubusercontent.com/radanalyticsio/equoid-ui/master/ocp/"
